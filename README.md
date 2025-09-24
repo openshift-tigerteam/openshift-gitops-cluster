94151d6a78ec816df89468aaca4b3c3b116c1ac5
snimmo <stephennimmo@gmail.com>
2025-09-24 08:42:12 -0500

    project init

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # openshift-gitops-cluster
+
+## Install Openshift GitOps
+
+```bash
+./install-openshift-gitops.sh
+```
---
+++ install-openshift-gitops.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+cd "$(dirname "${BASH_SOURCE[0]}")"
+set -euo pipefail
+
+log() {
+  echo "$(date '+%Y-%m-%d %H:%M:%S') $1 ${@:2}"
+}
+
+log "[INFO] Applying OpenShift GitOps operator subscription manifests..."
+oc apply -k openshift-gitops-operator
+
+log "[INFO] Waiting for the OpenShift GitOps Operator to be available..."
+until oc wait --for=condition=Available --timeout=300s deployment/openshift-gitops-operator-controller-manager -n openshift-gitops-operator >/dev/null 2>&1; do
+  log "[INFO] Waiting..."
+  sleep 10
+done
+log "[INFO] OpenShift GitOps Operator is now available."
+
+log "[INFO] Applying OpenShift GitOps instance openshift-gitops-cluster manifests..."
+oc apply -k openshift-gitops-cluster
+
+log "[INFO] Waiting for deployment/openshift-gitops-cluster-server to be created..."
+until oc get deployment openshift-gitops-cluster-server -n openshift-gitops-cluster >/dev/null 2>&1; do
+  log "[INFO] Still waiting for deployment/openshift-gitops-cluster-server to be created..."
+  sleep 10
+done
+
+# Verify ArgoCD is running
+log "[INFO] Waiting for deployment/openshift-gitops-cluster-server to be ready..."
+oc wait --for=condition=Available --timeout=300s deployment/openshift-gitops-cluster-server -n openshift-gitops-cluster
+log "[INFO] OpenShift GitOps successfully installed in openshift-gitops-cluster."
+
+ARGOCD_ROUTE=$(oc get route openshift-gitops-cluster-server -n openshift-gitops-cluster -o jsonpath='{.spec.host}')
+log "[INFO] Cluster Argo CD is accessible at: https://$ARGOCD_ROUTE"
